5c86fe94d6a880ef0db7cbff4b76aa740d2d8ed7
Student <example@example.com>
2020-02-14 14:55:22 -0800

    fix response for symptoms

--- src/main.js
+++ src/main.js
@@ -22,19 +22,35 @@ $(document).ready(function() {
       const nameArr = [];
       if (response) {
         for (let i = 0; i < docArr.length; i++) {
-          nameArr.push(`<li> ${response.data[i].profile.first_name} ${response.data[i].profile.last_name} - ${response.data[i].practices[i].visit_address.street} - ${response.data[i].practices[i].accepts_new_patients} </li>`)
+          nameArr.push(`<li> ${response.data[i].profile.first_name} ${response.data[i].profile.last_name} - ${response.data[i].practices[i].visit_address.street} - ${response.data[i].practices[i].accepts_new_patients} </li>`);
         
         }
       }
-
-
-
-      console.log(response)
-      $("#name").append(nameArr)
-
-    
-    
+      $("#name").append(nameArr);
     }
   });
+});
 
+$("#docInfo").submit(function(event) {
+  event.preventDefault();
+  const symps = $("#symptoms").val();
+  $("#symptoms").val("");
+
+  (async () => {
+    let doctorSymp = new DoctorSymp();
+    const response2 = await doctorSymp.getSymp(symps);
+    getElements(response2);
+  })();
+
+  function getElements(response2) {
+    const docArr2 = response2.data;
+    const sympArr = [];
+    if (response2) {
+      for (let j = 0; j < docArr2.length; j++) {
+        sympArr.push(`<li> ${response2.data[j].profile.first_name} ${response2.data[j].profile.last_name} - ${response2.data[j].practices[j].accepts_new_patients} </li>`);
+      }
+    }
+    console.log(sympArr)
+    $("#name").append(sympArr);
+  }
 });
