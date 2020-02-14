07f1880a649fdc4f31583ec654f0872658c23716
Student <example@example.com>
2020-02-14 09:56:34 -0800

    add jsonresponse in serv

--- src/main.js
+++ src/main.js
@@ -5,6 +5,24 @@ import './styles.css';
 import { DoctorInfo } from './../src/project-service.js';
 
 $(document).ready(function() {
-  
+  $("#docInfo").submit(function(event) {
+    event.preventDefault();
+    const prob = $("#symptom").val();
+    $("#symptom").val("");
 
-})
+    (async () => {
+      let doctorInfo = new DoctorInfo();
+      const response = await doctorInfo.getInfo(prob);
+      getElements(response);
+    })();
+
+    function getElements(response) {
+      if (response) {
+        $(`#name").append("Based off of your symptoms: ${prob}, we recommend the following doctors.  <li> ${response.data.first_name} ${response.data.last_name}`);
+      }
+    }
+
+
+  });
+
+});
--- src/project-service.js
+++ src/project-service.js
@@ -2,10 +2,15 @@ export class DoctorInfo {
 	async getInfo(info) {
 			try {
 				let response = await fetch(`https://api.betterdoctor.com/2016-03-01/doctors?location=37.773%2C-122.413%2C100&user_location=37.773%2C-122.413&skip=0&limit=10&user_key=2f43b27dfaff1f12faada478a028be97`);
-				let jsonifiedResponse = await response.json(); 
-				return jsonifiedResponse; 
-			} catch(error) {
-				console.error("I'm sorry, we cannot process your request.  We received an error: " + error.message);
+				let jsonifiedResponse;
+				if (response.ok && response.status == 200) {
+					jsonifiedResponse = await response.json();
+				} else {
+					jsonifiedResponse = false; 
+				}
+				return jsonifiedResponse;
+			 	} catch {
+					 return false;
+				 }
 			}
 		}
-	}
