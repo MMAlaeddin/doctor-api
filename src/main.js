4eeee70bbce71d94923b390c3874716f93519514
Student <example@example.com>
2020-02-14 08:47:15 -0800

    import export jquery and add some html

---
+++ src/main.js
@@ -0,0 +1,10 @@
+import $ from 'jquery';
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './styles.css';
+import { DoctorInfo } from './../src/project-service.js';
+
+$(document).ready(function() {
+  
+
+})
