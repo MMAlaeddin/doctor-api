a54e371d3ad1a39e123d743dc33aaf016a457083
Student <example@example.com>
2020-02-14 13:13:35 -0800

    update api to portland only

--- src/project-service.js
+++ src/project-service.js
@@ -1,7 +1,7 @@
 export class DoctorInfo {
 	async getInfo(prob) {
 			try {
-				let response = await fetch(`https://api.betterdoctor.com/2016-03-01/doctors?location=37.773%2C-122.413%2C100&user_location=37.773%2C-122.413&skip=0&limit=10&user_key=2f43b27dfaff1f12faada478a028be97`);
+				let response = await fetch(`https://api.betterdoctor.com/2016-03-01/doctors?location=or-portland&user_location=37.773%2C-122.413&skip=0&limit=10&user_key=2f43b27dfaff1f12faada478a028be97`);
 				let jsonifiedResponse;
 				if (response.ok && response.status == 200) {
 					jsonifiedResponse = await response.json();
