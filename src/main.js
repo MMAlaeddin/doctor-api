01ec61dcfa5b0b9f1cb438717043d45fd1c3f852
Student <example@example.com>
2020-02-14 16:20:39 -0800

    add error outputs

--- src/main.js
+++ src/main.js
@@ -20,12 +20,15 @@ $(document).ready(function() {
     function getElements(response) {
       const docArr = response.data;
       const nameArr = [];
-      if (response) {
+      if (response.data.length === 0) {
+        alert("Sorry, no results for that.  Please try again.")
+      } else if (response) {
         for (let i = 0; i < docArr.length; i++) {
           nameArr.push(`<li> ${response.data[i].profile.first_name} ${response.data[i].profile.last_name} - ${response.data[i].practices[i].visit_address.street} - ${response.data[i].practices[i].accepts_new_patients} </li>`);
+ 
         
-        }
       }
+    }
       $("#name").append(nameArr);
     }
   });
@@ -45,7 +48,9 @@ $("#sympForm").submit(function(event) {
   function getElements2(response2) {
     const docArr2 = response2.data;
     const sympArr = [];
-    if (response2) {
+    if (response2.data.length === 0) {
+      alert("Sorry, no results for that.  Please try again.")
+    } else if (response2) {
       for (let j = 0; j < docArr2.length; j++) {
         sympArr.push(`<li> ${response2.data[j].profile.first_name} <br> ${response2.data[j].profile.last_name} <br> ${response2.data[j].practices[0].visit_address.street} <br> ${response2.data[j].practices[0].accepts_new_patients} <br> <a href="${response2.data[j].practices[0].website}">Link to website</a> </li>`);
       }
--- src/project-service.js
+++ src/project-service.js
@@ -1,7 +1,7 @@
 export class DoctorInfo {
 	async getInfo(name) {
 			try {
-				let response = await fetch(`https://api.betterdoctor.com/2016-03-01/doctors?name=${name}&location=or-portland&user_location=37.773%2C-122.413&skip=0&limit=10&user_key=2f43b27dfaff1f12faada478a028be97`);
+				let response = await fetch(`https://api.betterdoctor.com/2016-03-01/doctors?name=${name}&location=or-portland&user_location=37.773%2C-122.413&skip=0&limit=10&user_key=${process.env.API_KEY}`);
 				let jsonifiedResponse;
 				if (response.ok && response.status == 200) {
 					jsonifiedResponse = await response.json();
@@ -17,7 +17,7 @@ export class DoctorInfo {
     export class DoctorSymp {
       async getSymp(symps) {
           try {
-            let response = await fetch(`https://api.betterdoctor.com/2016-03-01/doctors?query=${symps}&location=or-portland&user_location=37.773%2C-122.413&skip=0&limit=10&user_key=2f43b27dfaff1f12faada478a028be97`);
+            let response = await fetch(`https://api.betterdoctor.com/2016-03-01/doctors?query=${symps}&location=or-portland&user_location=37.773%2C-122.413&skip=0&limit=10&user_key=${process.env.API_KEY}`);
             let jsonifiedResponse;
             if (response.ok && response.status == 200) {
               jsonifiedResponse = await response.json();
@@ -30,4 +30,3 @@ export class DoctorInfo {
              }
           }
         }
-        
